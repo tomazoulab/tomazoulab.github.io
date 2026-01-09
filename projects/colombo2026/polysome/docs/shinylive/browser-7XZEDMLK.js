592de7fc8ca02b6fd22500aa9837e12ac9a74688
Christoph Dotter <christoph.dotter@gmail.com>
2026-01-09 11:26:26 +0100

    add shiny app

---
+++ projects/colombo2026/polysome/docs/shinylive/browser-7XZEDMLK.js
@@ -0,0 +1,3 @@
+// Shinylive 0.9.2
+// Copyright 2024 Posit, PBC
+import{c as t}from"./chunk-Y6TCCHOT.js";var o=t((r,e)=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}});export default o();
